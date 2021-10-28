afbac64de01da3b9ff1d7d2fca95d39302d4cec5
MarcRobichaud <marc_robi@yahoo.ca>
2021-10-28 10:28:21 -0400

    done

--- Assets/Scripts/SimpleController.cs
+++ Assets/Scripts/SimpleController.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 
+public enum GameState
+{
+    PlacingBall,
+    DroppingBall,
+    GameOver
+}
+
 [RequireComponent(typeof(Rigidbody))]
 public class SimpleController : MonoBehaviour
 {
     public float speed = 1f;
+    public GameState gameState = GameState.PlacingBall;
 
     private Rigidbody rb;
     private Vector3 initialPosition;
+
+    private float leftBounds = 1.7f;
+    private float rightBounds = -1.75f;
 
     private void Awake()
     {
@@ -16,31 +27,78 @@ public class SimpleController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) && !rb.useGravity)
+        //SW TAB TAB varname enter
+        switch (gameState)
+        {
+            case GameState.PlacingBall:
+                PlacingBallUpdate();
+                break;
+
+            case GameState.DroppingBall:
+                break;
+
+            case GameState.GameOver:
+                GameOverUpdate();
+                break;
+
+            default:
+                break;
+        }
+
+        if (gameState == GameState.GameOver)
+        {
+        }
+    }
+
+    private void GameOverUpdate()
+    {
+        if (Input.anyKeyDown)
+        {
+            gameState = GameState.PlacingBall;
+            transform.position = initialPosition;
+            rb.useGravity = false;
+            rb.isKinematic = true;
+        }
+    }
+
+    private void PlacingBallUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             Vector3 pos = transform.position;
             pos.x += speed * Time.deltaTime;
+
+            if (pos.x > leftBounds)
+            {
+                pos.x = leftBounds;
+            }
+
             transform.position = pos;
         }
-        else if (Input.GetKey(KeyCode.RightArrow) && !rb.useGravity)
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             Vector3 pos = transform.position;
             pos.x -= speed * Time.deltaTime;
+
+            if (pos.x < rightBounds)
+            {
+                pos.x = rightBounds;
+            }
+
             transform.position = pos;
         }
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (!rb.useGravity)
-            {
-                rb.useGravity = true;
-                rb.isKinematic = false;
-            }
-            else
-            {
-                rb.useGravity = false;
-                rb.isKinematic = true;
-                transform.position = initialPosition;
-            }
+            gameState = GameState.DroppingBall;
+            rb.useGravity = true;
+            rb.isKinematic = false;
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameState = GameState.GameOver;
+        AudioSource audio = other.GetComponent<AudioSource>();
+        audio.Play();
     }
 }
